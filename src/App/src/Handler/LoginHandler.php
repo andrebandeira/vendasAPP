f1064a5d41a77b98aac7c27ea51f49db5f0c8288
ANDRE LUIS MARTINS BANDEIRA <andre@debian.bandeira>
2019-06-01 13:09:55 -0300

    init

---
+++ src/App/src/Handler/LoginHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\View\Model\ViewModel;
+
+
+class LoginHandler implements RequestHandlerInterface
+{
+    private $template;
+    public function __construct(
+        TemplateRendererInterface $template = null
+    ) {
+        $this->template = $template;
+    }
+
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        return new HtmlResponse($this->template->render('app::login'));
+    }
+}
