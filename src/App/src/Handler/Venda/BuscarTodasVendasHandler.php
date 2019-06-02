ead1df716f2d0e13e5a8138852d2dee4157937b5
ANDRE LUIS MARTINS BANDEIRA <andre@debian.bandeira>
2019-06-01 23:06:39 -0300

    adicionado o filtro do periodo

--- src/App/src/Handler/Venda/BuscarTodasVendasHandler.php
+++ src/App/src/Handler/Venda/BuscarTodasVendasHandler.php
@@ -20,12 +20,21 @@ class BuscarTodasVendasHandler extends MainHandler
         try {
             $token = $this->getHeader($request, 'token');
 
+            $dataInicio = $this->getPost($request, 'data-inicio');
+            $dataFim = $this->getPost($request, 'data-fim');
+
             $client = new Client();
             $client->setMethod(Request::METHOD_GET);
             $client->setUri('http://127.0.0.1/api/venda');
             $client->setHeaders([
                 'token' => $token
             ]);
+            $client->setParameterGet(
+                [
+                    'data-inicio' => $dataInicio,
+                    'data-fim'    => $dataFim
+                ]
+            );
             $result   = $client->send();
             $body     = $result->getBody();
             $resposta = json_decode($body);
